ec3c80cfa073fc605888c875413292963c8106d0
Aariz-Khan <52038076+Aariz-Khan@users.noreply.github.com>
2023-07-27 04:12:21 +0530

    Add files via upload

---
+++ Sprites/Tiles for TILED/GrassFloorDetail.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="GrassFloorDetail" tilewidth="64" tileheight="64" tilecount="80" columns="16">
+ <image source="../1 - level/graphics/tilemap/details.png" width="1024" height="320"/>
+</tileset>
---
+++ Sprites/Tiles for TILED/Ruin Walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Ruin Walls" tilewidth="64" tileheight="64" tilecount="256" columns="16">
+ <image source="../Texture/TX Tileset Wall 64px.png" width="1024" height="1024"/>
+</tileset>
